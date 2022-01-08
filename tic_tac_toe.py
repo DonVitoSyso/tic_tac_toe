db444b76da84ff7f70c5f3e555815989f5327dcb
DonVito <vitoss32@mail.ru>
2022-01-08 17:30:21 +0300

    Корректировка_3 после просмотра вебинара. Заключительная

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -9,17 +9,19 @@ def check_step(result, op):
     for i in range(3):
         if result[i][0] == result[i][1] == result[i][2] != '-' or \
                 result[0][i] == result[1][i] == result[2][i] != '-' or \
-                result[0][0] == result[1][1] == result[2][2] != '-' or \
-                result[0][2] == result[1][1] == result[2][0] != '-':
+                result[0][i] == result[1][i] == result[2][i] != '-' or \
+                result[0][2-i] == result[1][2-i] == result[2][2-i] != '-':
             print("Выиграл ", op)
             return True
     return False
+
 
 def input_step(fld, str_, value):
 
     #запись хода в таблицу
     while True:
-        if 0 <= int(str_[0]) <= 2 and 0 <= int(str_[1]) <= 2 \
+        if (str_[0].isdigit() and str_[1].isdigit()) and \
+                0 <= int(str_[0]) <= 2 and 0 <= int(str_[1]) <= 2 \
                 and fld[int(str_[0])][int(str_[1])] == '-':
             fld[int(str_[0])][int(str_[1])] = value
             return fld[int(str_[0])][int(str_[1])]
@@ -41,9 +43,9 @@ for i in range(9):
         input_step(field, str_, 'o')
         str_ = 'Противник 2 (o)!'
     else:
-        str_ = input("Ход противника 1 (х): ")
+        str_ = input("Ход противника 1 (x): ")
         input_step(field, str_, 'x')
-        str_ = 'Противник 1 (х)!'
+        str_ = 'Противник 1 (x)!'
 
     if check_step(field, str_):
         break
