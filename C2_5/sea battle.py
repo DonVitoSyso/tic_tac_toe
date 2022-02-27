434246ec1bde8b8dc1c42ee0067cca18c4b8bfcd
DonVito <vitoss32@mail.ru>
2022-02-27 13:47:51 +0300

    ИТОГОВОЕ ЗАДАНИЕ 1.6 (HW-03)

--- C2_5/sea battle.py
+++ C2_5/sea battle.py
@@ -43,34 +43,35 @@ class Ship():
         dots_ = []
         if self.orientation == "H":
             for h in range(self.length):
-                dots_.append(Dot(self.ship_bow.x + h, self.ship_bow.y, '+'))
+                dots_.append(Dot(self.ship_bow.x, self.ship_bow.y + h, '+'))
             # dots_.append(list(Dot(self.ship.x + h, self.ship.y, '+')) for h in range(0, self.length))
         elif self.orientation == "V":
             for v in range(self.length):
-                dots_.append(Dot(self.ship_bow.x, self.ship_bow.y + v, '+'))
+                dots_.append(Dot(self.ship_bow.x + v, self.ship_bow.y, '+'))
             # dots_ = Dot((self.ship.dot[0], self.ship.dot[1] + v, '+') for v in range(0, self.length))
         return dots_
     # проверка поподания выстрела в точки коробля
     def shooten(self, shot):
         return shot in self.dots
 
-# класс написан полностью, БЕЗ ОПИСАНИЯ ВЫСТРЕЛОВ
+# класс написан полностью
 class Board():
     def __init__(self, hid=False, size=6):
         self.board_ = [["o" for _ in range(size)] for _ in range(size)]
         self.ships = []
         self.hid = hid
         self.size = size
+        # колличество параженных короблей
         self.count = 0
-        # этот тип непонятен
+        # количество занятых точек
         self.busy = []
-
+    # начало игры обнуляем список busy
     def begin(self):
         self.busy = []
 
     def out(self, dot):
-        return False if 0 <= dot.x < 6 and 0 <= dot.y < 6 else True
-    # зделан полностью, и контур тоже
+        return False if 0 <= dot.x < self.size and 0 <= dot.y < self.size else True
+    # сделан полностью, и контур тоже
     def add_ship(self, ship):
         # # генераторы случайных расположений
         # ornt = random.choice("HV")
@@ -87,7 +88,7 @@ class Board():
             self.busy.append(i)
         self.ships.append(ship)
         self.contour(ship)
-    # полностью скопирован из вэб
+    # полностью скопирован из вэб + разобрался
     def contour(self, ship, verb=False):
         near = [
             (-1, -1), (-1, 0), (-1, 1),
@@ -101,35 +102,61 @@ class Board():
                     if verb:
                         self.board_[cur.x][cur.y] = "."
                     self.busy.append(cur)
-
+    # мой метод
     def show(self):
-        # print("   | 1 | 2 | 3 | 4 | 5 | 6 |")
-        # print("---+---+---+---+---+---+---+")
-        # for i in range(0, 6):
-        #     print(f' {i+1} ', end='|')
-        #     for j in range(0, 6):
-        #         print(f' {self.board_[j].dot[0]} ', end='|')
-        #     print('\n---+---+---+---+---+---+---+')
-        print("    1  2  3  4  5  6 ")
-        for i in range(0, 6):
-            print(f' {i + 1} ', end='')
-            for j in range(0, 6):
-                print(f' {self.board_[i][j].val} ', end='')
-            print()
+        res = ""
+        res += "    1  2  3  4  5  6"
 
-    def shot(self):
-        ...
+        for i, j in enumerate(self.board_):
+            res += f"\n {i+1}  " + "  ".join(j)
+        # Разобраться надо!!!
+        if self.hid:
+            res = res.replace("■", "o")
+        return res
+    # скопирован из кода - всё понятно
+    def shot(self, dot):
 
-class Player(Board):
+        if self.out(dot):
+            raise BoardOutException()
+
+        if dot in self.busy:
+            raise BoardUsedException()
+
+        self.busy.append(dot)
+
+        for ship in self.ships:
+            if ship.shooten(dot):
+                ship.lives -= 1
+                self.board_[dot.x][dot.y] = "X"
+                if ship.lives == 0:
+                    self.count += 1
+                    self.contour(ship, verb=True)
+                    print("Корабль уничтожен!")
+                    return False
+                else:
+                    print("Корабль ранен!")
+                    return True
+
+        self.board_[dot.x][dot.y] = "."
+        print("Мимо!")
+        return False
+
+class Player:
     def __init__(self, board_player1, board_player2):
         self.board_player1 = board_player1
         self.board_player2 = board_player2
 
     def ask(self):
         raise NotImplementedError()
-
+    # скопирован из ВЭБинара - разобран
     def move(self):
-        ...
+        while True:
+            try:
+                target = self.ask()
+                repeat = self.board_player2.shot(target)
+                return repeat
+            except BoardException as e:
+                print(e)
 # класс пользователь закончен - вводимые значения
 class User(Player):
     def ask(self):
@@ -148,11 +175,10 @@ class User(Player):
 class AI(Player):
     def ask(self):
         d = Dot(random.randint(0, 5), random.randint(0, 5))
-        print(f'Ход AI:({d.x}, {d.y})')
+        print(f'Ход AI:({d.x + 1}, {d.y + 1})')
         return d
-
-# класс Game осталось завести loop
-class Game(Player):
+# класс Game закончен + скопирован + разобран
+class Game:
     def random_board(self):
         ship_list = [3, 2, 2, 1, 1, 1, 1]
         board = Board(size=self.size)
@@ -162,7 +188,11 @@ class Game(Player):
             if i == len(ship_list):
                 break
             if cont > 2000:
-                i = 0
+                # Переинициализация всего. Проблема зависания
+                # переменно board при генерации свыше N-ой операции
+                i, cont = 0, 0
+                del board
+                board = Board(size=self.size)
             # генератор случайных расположений
             ship = Ship(Dot(random.randint(0, self.size), random.randint(0, self.size)), ship_list[i],
                         random.choice("HV"))
@@ -174,61 +204,61 @@ class Game(Player):
                 pass
         board.begin()
         return board
-
-
+    # скопирован из вэбинара + разобрался
     def __init__(self, size=6):
-        board1 = random_board()
-        board2 = random_board()
-        self.player1 = User(board1, board2)
-        self.player2 = AI(board2, board1)
-        # параметры для всех классов объявлены size, hid
         self.size = size
-        self.hid = True
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
 
-    # greet закончен подсказка на игру
-    def greet():
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
+    # скопирован из вэбинара + разобрался
+    def greet(self):
         print("-------------------")
         print("  Приветсвуем вас  ")
+        print("      в игре       ")
+        print("    морской бой    ")
+        print("-------------------")
         print(" формат ввода: x y ")
-
+        print(" x - номер строки  ")
+        print(" y - номер столбца ")
+    # скопирован из вэбинара + разобрался
     def loop(self):
-        ...
-    # метод start описан
+        num = 0
+        while True:
+            print("-" * 20)
+            print("Доска пользователя:")
+            print(self.us.board_player1.show())
+            print("-" * 20)
+            print("Доска компьютера:")
+            print(self.ai.board_player1.show())
+            print("-" * 20)
+            if num % 2 == 0:
+                print("Ходит пользователь!")
+                repeat = self.us.move()
+            else:
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board_player1.count == 7:
+                print("-" * 20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board_player1.count == 7:
+                print("-" * 20)
+                print("Компьютер выиграл!")
+                break
+            num += 1
+   # скопирован из вэбинара + разобрался
     def start(self):
         self.greet()
         self.loop()
 
 
-board_test = [[] for _ in range(0, 6)]
-print(board_test)
-print(len(board_test))
-dot_ = Dot(2, 3)
-ship = Ship(3, dot_, "V")
-
-#print(ship.get_Ship(), ship.get_Dot())
-#print(*ship.dots())
-board = Board(ship, True, 1)
-print(board.ships.ship.get_Dot())
-# print(board.board_)
-# board.show()
-board.show()
-ship_list = [1, 2, 2, 1, 1, 1, 1]
-for s in ship_list:
-    print(board.add_ship(s))
-board.show()
-
-list_1 = [Dot(y, x) for x, y in zip(range(4), range(4))]
-print(list_1)
-
-list_2 = [Dot(0,0), Dot(1,1), Dot(1,2)]
-
-# print(list(filter(lambda i: i in list_2, list_2)))
-if all(x in list_1 for x in list_2):
-    print("Проверка прошла!")
-else:
-    print(False)
-
-
 # сама игра начинатся здесь
-game = Game
-game.greet()
+game = Game()
+game.start()
